c512311ca9378db592ea54c1a2ed8e7859ad8bfe
becarpio <50060813+becarpio@users.noreply.github.com>
2020-09-11 12:36:56 -0700

    Updated A5 name and email address

--- roster.cpp
+++ roster.cpp
@@ -211,7 +211,7 @@ void main() {
 			"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 			"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 			"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-			"A5,Bethany,Carpio,bcarpio@my.wgu.edu,29,20,50,42,SOFTWARE" };
+			"A5,Jane,Smith,JSmith@school.edu,29,20,50,42,SOFTWARE" };
 
 	std::string updatedStudentData;
 	std::string currentID;
